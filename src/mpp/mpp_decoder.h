030347ce01c66d1e2c646bc8498856b624ce12a9
xiao-sheng-av <13435249798@163.com>
2025-08-13 15:34:09 +0800

    增加获取高度等信息的成员函数

--- src/mpp/mpp_decoder.h
+++ src/mpp/mpp_decoder.h
@@ -43,12 +43,14 @@ public:
 	MppDecoder(int w = 1920, int h = 1080) : width(w), height(h) {};
 	~MppDecoder();
 	bool Init();
+	bool Group_Init(AVPacket * av_packet);
 	bool packet_Init(AVPacket * av_packet);
 	bool Decode();
-	bool Group_Init(AVPacket * av_packet);
 	bool Write_File(FILE * f);
 	RK_U32 get_hor() const {return hor_stride;}
 	RK_U32 get_ver() const {return ver_stride;}
+	RK_U32 get_width() const {return width;}
+	RK_U32 get_height() const {return height;}
 	void Frame_Deinit();
 };
 
